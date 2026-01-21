47218d8d7cad082ca99858eec782ac6e1f3459f1
P01Sensai <pramanshuprajapati@gmil.com>
2026-01-21 23:08:50 +0530

    Implement math game functionality with addition, subtraction, multiplication, and division modes; add menu and game templates.

--- app.py
+++ app.py
@@ -4,29 +4,57 @@ import random
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-  
-    num1 = random.randint(1, 10)
-    num2 = random.randint(1, 10)
- 
-    correct_answer = num1 + num2
+def menu():
+    return render_template('menu.html')
 
-    return render_template('index.html', n1=num1, n2=num2, real_ans=correct_answer)
+@app.route('/play/<mode>')
+def start_game(mode):
+    if mode == 'mix':
+        current_op = random.choice(['add', 'sub', 'mul', 'div'])
+    else:
+        current_op = mode
+
+    if current_op == 'add':
+        n1 = random.randint(1, 20)
+        n2 = random.randint(1, 20)
+        symbol = "+"
+        ans = n1 + n2
+        
+    elif current_op == 'sub':
+        n1 = random.randint(10, 30)
+        n2 = random.randint(1, 10)
+        if n2 > n1: n1, n2 = n2, n1 
+        symbol = "-"
+        ans = n1 - n2
+        
+    elif current_op == 'mul':
+        n1 = random.randint(1, 10)
+        n2 = random.randint(1, 10)
+        symbol = "×"
+        ans = n1 * n2
+        
+    elif current_op == 'div':
+        
+        ans = random.randint(1, 10) 
+        n1 = n2 * ans
+        symbol = "÷"
+
+    return render_template('game.html', n1=n1, n2=n2, symbol=symbol, real_ans=ans, mode=mode)
 
 @app.route('/check', methods=['POST'])
 def check_answer():
-
     user_guess = int(request.form['guess'])
     real_answer = int(request.form['real_ans'])
-
+    mode = request.form['mode'] 
+    
     if user_guess == real_answer:
-        msg = "Correct! Great Job! 🎉"
+        msg = "Correct! You're a Star! 🌟"
         color = "green"
     else:
         msg = f"Oops! The answer was {real_answer}. 😅"
         color = "red"
-  
-    return render_template('result.html', message=msg, color=color)
+        
+    return render_template('result.html', message=msg, color=color, mode=mode)
 
 if __name__ == '__main__':
     app.run(debug=True)

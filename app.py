cfbe1424960bed0bd9f1891df97d98cb2f10c9b7
P01Sensai <pramanshuprajapati@gmil.com>
2026-01-21 18:42:55 +0530

    Switched to Math Game

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return render_template('index.html', dice_value="?")
+  
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+ 
+    correct_answer = num1 + num2
 
-@app.route('/roll')
-def roll_dice():
-    result = random.randint(1, 6)
-    print(f'Dice rolled: {result}')
-    return render_template('index.html', dice_value=result)
+    return render_template('index.html', n1=num1, n2=num2, real_ans=correct_answer)
+
+@app.route('/check', methods=['POST'])
+def check_answer():
+
+    user_guess = int(request.form['guess'])
+    real_answer = int(request.form['real_ans'])
+
+    if user_guess == real_answer:
+        msg = "Correct! Great Job! 🎉"
+        color = "green"
+    else:
+        msg = f"Oops! The answer was {real_answer}. 😅"
+        color = "red"
+  
+    return render_template('result.html', message=msg, color=color)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
